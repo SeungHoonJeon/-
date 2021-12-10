4182cb076456002b7b78a3de8a4d9486a04ec041
TunaF <tmdgns0903@naver.com>
2021-12-10 19:22:18 +0900

    4.연산자

--- java_basic_test/MainClass.java
+++ java_basic_test/MainClass.java
@@ -2,49 +2,52 @@ package java_basic_test;
 
 public class MainClass {
 	public static void main(String[] args) {
-		System.out.println("Good");
-		System.out.printf("Good");
-		System.out.printf("Good");
+		int x = 10;
+		int y= 20;
+		System.out.println("x= "+x);
+		System.out.println("y= "+y);
 		
-		System.out.println("good\tgood\tgood");
-		System.out.println("goo    d\ngood");
-		System.out.println("seunghoon : \"good\"");
-		System.out.println("seunghoon : \'good\'");
-		System.out.println("seunghoon : \\good\\");
+		x = y;
 		
-		System.out.println("----------------------------");
-		int a = 10;
-		float b = 5.2f;
-		double c = 1.128547d;
-		char d = 'a';
-		String s = "hello";
+		System.out.println("x="+x);
 		
+		System.out.println("x+y="+(x+y));
+		System.out.println("x-y="+(x-y));
+		System.out.println("x*y="+(x*y));
+		System.out.println("x/y="+(x/y));
+		System.out.println("x%y="+(x%y));
 		
-		System.out.println("서식문자테스트");
-		//System.out.println("서식문자테스트  %d", a); // 이렇게 서식문자 쓰면 안됨. //error 
-		System.out.printf("서식문자테스트 %d \n", a);
-		System.out.printf("서식문자테스트 %o \n", a);
-		System.out.printf("서식문자테스트 %x \n", a);
+		x+=10;
+		System.out.printf("x = %d \n", x);
 		
-		/* error => 서식문자 형에 맞게끔 써야함.
-		System.out.printf("서식문자테스트 %s \n", a);
-		System.out.printf("서식문자테스트 %f \n", a);
-		*/
+		System.out.println("x>y : " + (x>y));
+		System.out.println("x<y : " + (x<y));
+		System.out.println("x>=y : " + (x>=y));
+		System.out.println("x<=y : " + (x<=y));
+		System.out.println("x==y : " + (x==y));
+		System.out.println("x!=y : " + (x!=y));
 		
-		System.out.println("----------------------------");
-		System.out.printf("서식문자테스트 %d \n", a);
-		System.out.printf("서식문자테스트 %f \n", b);
-		System.out.printf("서식문자테스트 %f \n", c);
-		System.out.printf("서식문자테스트 %c \n", d);
-		System.out.printf("서식문자테스트 %s \n", s);
+		x++;
+		System.out.println("x ="+x);
 		
+		boolean b1 = false;
+		boolean b2 = true;
 		
-		System.out.println("----------------------------");
-		System.out.printf("서식문자테스트 %5d \n", a);
-		System.out.printf("서식문자테스트 %.0f \n", b);
-		System.out.printf("서식문자테스트 %.1f \n", c);
-		System.out.printf("서식문자테스트 %c \n", d);
-		System.out.printf("서식문자테스트 %s \n", s);
+		System.out.println("b1 && b2 : "+(b1&&b2));
+		System.out.println("b1 || b2 : "+(b1||b2));
+		System.out.println("!b1 : "+(!b1));
+		System.out.println("!b2 : "+(!b2));
+		
+		x = 10; y = 20;
+		int result = 0;
+		result = (x>y)?100:200;
+		System.out.println("result: "+ result);
+		
+		result = (x<y)?100:200;
+		System.out.println("result: "+ result);
+		
+		result = (x==y)?100:200;
+		System.out.println("result: "+ result);
 		
 	}
 }
