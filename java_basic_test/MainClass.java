6f75e6a45405ccacb5c681073e482de3eb2edea3
TunaF <tmdgns0903@naver.com>
2021-12-10 16:38:43 +0900

    3.특수문자,서식문자

--- java_basic_test/MainClass.java
+++ java_basic_test/MainClass.java
@@ -2,22 +2,49 @@ package java_basic_test;
 
 public class MainClass {
 	public static void main(String[] args) {
-		byte a = 10;
-		int b = 10;
+		System.out.println("Good");
+		System.out.printf("Good");
+		System.out.printf("Good");
 		
-		System.out.println(a);
-		System.out.println(b);
+		System.out.println("good\tgood\tgood");
+		System.out.println("goo    d\ngood");
+		System.out.println("seunghoon : \"good\"");
+		System.out.println("seunghoon : \'good\'");
+		System.out.println("seunghoon : \\good\\");
 		
-		int c = 100; //0110 0100
-		byte c_1 = (byte)c;
-		System.out.println(c_1);
+		System.out.println("----------------------------");
+		int a = 10;
+		float b = 5.2f;
+		double c = 1.128547d;
+		char d = 'a';
+		String s = "hello";
 		
-		int d = 1000000; // 1111 0100 0010 0100 0000
-		byte d_1 = (byte)d;
-		System.out.println(d_1); //1111 0100 0010 0100 0000 중에서 0100 0000만 처리함.  
 		
-		int f = 1000001; // 1111 0100 0010 0100 0001
-		byte f_1 = (byte)f;
-		System.out.println(f_1);  // 1111 0100 0010 0100 0001 중에서 0100 0001만 처리함.
+		System.out.println("서식문자테스트");
+		//System.out.println("서식문자테스트  %d", a); // 이렇게 서식문자 쓰면 안됨. //error 
+		System.out.printf("서식문자테스트 %d \n", a);
+		System.out.printf("서식문자테스트 %o \n", a);
+		System.out.printf("서식문자테스트 %x \n", a);
+		
+		/* error => 서식문자 형에 맞게끔 써야함.
+		System.out.printf("서식문자테스트 %s \n", a);
+		System.out.printf("서식문자테스트 %f \n", a);
+		*/
+		
+		System.out.println("----------------------------");
+		System.out.printf("서식문자테스트 %d \n", a);
+		System.out.printf("서식문자테스트 %f \n", b);
+		System.out.printf("서식문자테스트 %f \n", c);
+		System.out.printf("서식문자테스트 %c \n", d);
+		System.out.printf("서식문자테스트 %s \n", s);
+		
+		
+		System.out.println("----------------------------");
+		System.out.printf("서식문자테스트 %5d \n", a);
+		System.out.printf("서식문자테스트 %.0f \n", b);
+		System.out.printf("서식문자테스트 %.1f \n", c);
+		System.out.printf("서식문자테스트 %c \n", d);
+		System.out.printf("서식문자테스트 %s \n", s);
+		
 	}
 }
