0f68002ac8bb4c2b5b5388aff8418ba905c7a0ec
TunaF <tmdgns0903@naver.com>
2021-12-10 19:49:52 +0900

    6.배열 메모리

--- java_basic_test/MainClass.java
+++ java_basic_test/MainClass.java
@@ -1,57 +1,36 @@
 package java_basic_test;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class MainClass {
 	public static void main(String[] args) {
-		int[] arr = new int[5];
-		arr[0] = 0;
-		arr[1] = 1;
-		arr[2] = 2;
-		arr[3] = 3;
-		arr[4] = 4;
+		int arr1[] = {1,2,3,4,5};
+		int arr2[] = null;
+		int arr3[] = null;
 		
-		System.out.println("arr[0]:"+arr[0]);
-		System.out.println("arr[1]:"+arr[1]);
-		System.out.println("arr[2]:"+arr[2]);
-		System.out.println("arr[3]:"+arr[3]);
-		System.out.println("arr[4]:"+arr[4]);
-		//System.out.println("arr[0]:"+arr[5]); //err: Index 5 out of bounds for length 5
+		System.out.println("length of arr1 is "+arr1.length);
+		System.out.println("value of index is "+Arrays.toString(arr1));
 		
-		String[] name = {"전승훈","조윤성","박한수","지수용","박광렬","김경록"};
-		int[] score = new int[6];
-		float avg;
-		Scanner input = new Scanner(System.in); 
+		arr2 = Arrays.copyOf(arr1,arr1.length);
+		System.out.println("value of index is "+Arrays.toString(arr2));
+		System.out.println("address of arr2 is "+arr2);
+		System.out.println("address of arr1 is "+arr1); //address of arr1 is different its arr2
 		
-		System.out.printf("%s의 점수를 입력하시오. :",name[0]);
-		score[0] = input.nextInt();
+		arr3 = arr1;
+		System.out.println("address of arr3 is"+arr3);
+		System.out.println("address of arr1 is"+arr1); // address of arr1 is same its arr3
 		
-		System.out.printf("%s의 점수를 입력하시오. :",name[1]);
-		score[1] = input.nextInt();
 		
-		System.out.printf("%s의 점수를 입력하시오. :",name[2]);
-		score[2] = input.nextInt();
+		int[][] arr4 = new int[2][2];
+		arr4[0][0] = 1;
+		arr4[0][1] = 2;
+		arr4[1][0] = 3;
+		arr4[1][1] = 4;
 		
-		System.out.printf("%s의 점수를 입력하시오. :",name[3]);
-		score[3] = input.nextInt();
-		
-		System.out.printf("%s의 점수를 입력하시오. :",name[4]);
-		score[4] = input.nextInt();
-		
-		System.out.printf("%s의 점수를 입력하시오. :",name[5]);
-		score[5] = input.nextInt();
-		
-		System.out.printf("%s 점수 : %d \n", name[0], score[0]);
-		System.out.printf("%s 점수 : %d \n", name[1], score[1]);
-		System.out.printf("%s 점수 : %d \n", name[2], score[2]);
-		System.out.printf("%s 점수 : %d \n", name[3], score[3]);
-		System.out.printf("%s 점수 : %d \n", name[4], score[4]);
-		System.out.printf("%s 점수 : %d \n", name[5], score[5]);
-		
-		avg = (float)(score[0]+score[1]+score[2]+score[3]+score[4]+score[5]);
-		System.out.printf("-------------평균 : %.2f \n",avg);
-		
-		input.close();
-		
+		System.out.println(arr4[0][0]);
+		System.out.println(arr4[0][1]);
+		System.out.println(arr4[1][0]);
+		System.out.println(arr4[1][1]);
 	}
 }
