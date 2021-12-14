af8e4e4b397b20c95cdcddcf00a0ff0ec614c5fc
TunaF <tmdgns0903@naver.com>
2021-12-14 14:00:21 +0900

    this 키워드

--- java_basic_test/MainClass.java
+++ java_basic_test/MainClass.java
@@ -2,22 +2,7 @@ package java_basic_test;
 
 public class MainClass {
 	public static void main(String[] args) {
-		ObjectEx obj1 = new ObjectEx();
-		System.out.println();
-		
-		ObjectEx obj2 = new ObjectEx(10);
-		
-		int arr[] = {10,20,30};
-		ObjectEx obj3 = new ObjectEx("Java", arr);
-		
-		ObjectEx obj4;
-		
-		obj4 = new ObjectEx();
-		obj4 = new ObjectEx();
-		
-		System.gc();
-		
-		int ints[] = {10,20,30};
-		ObjectEx obj5 = new ObjectEx(10, "JAVA", ints);
+		ObjectClass obj1 = new ObjectClass(10,20);
+		obj1.getInfo();
 	}
 }
