2ce9a98af3a8db389021c31da8d7733d041dbffa
TunaF <tmdgns0903@naver.com>
2021-12-18 00:23:09 +0900

    익명클래스

---
+++ java_basic_test/AnonymousClass.java
@@ -0,0 +1,10 @@
+package java_basic_test;
+
+public class AnonymousClass {
+	public AnonymousClass() {
+		System.out.println("AnonymousClass constructor");
+	}
+	public void method() {
+		System.out.println("--AnnonymousClass's method START--");
+	}
+}
--- java_basic_test/MainClass.java
+++ java_basic_test/MainClass.java
@@ -2,9 +2,10 @@ package java_basic_test;
 
 public class MainClass {
 	public static void main(String[] args) {
-		FirstChildClass childClass = new FirstChildClass();
-		childClass.makeJJajang();
-		
-		
+		new AnonymousClass(){
+			public void method() {
+				System.out.println("--AnonymousClass's Override method START");
+			};
+		}.method();
 	}
 }
