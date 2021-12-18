c16346e3e849cfdcab6e1bc4cc8d92dfe95bc1a0
TunaF <tmdgns0903@naver.com>
2021-12-18 13:24:24 +0900

    리스트(1)

--- java_basic_test/MainClass.java
+++ java_basic_test/MainClass.java
@@ -1,27 +1,38 @@
 package java_basic_test;
 
+import java.util.ArrayList;
 
 public class MainClass {
 	public static void main(String[] args) {
-		String str = "JAVA";
-		//String str = new String("Java"); 이렇게도 써도 되지만 자주 쓰이는 클래스이기 떄문에 간단하게 쓸 수 있게 이렇게 만듬
-		System.out.println("str: "+str);
+		ArrayList<String> list = new ArrayList<String>();
 		
-		str = str + "_8";
-		System.out.println("str"+ str);
+		System.out.println("list.size : "+list.size());
 		
-		StringBuffer sf = new StringBuffer("JAVA");
-		System.out.println("sf : "+sf);
-		sf.append("World");
-		System.out.println("sf: " +sf);
-		System.out.println("sf.length():"+sf.length());
-		sf.insert(sf.length(), "~~~~"); //매개변수1 : 삽입할 위치, 매개변수2: 삽일 할 문자열
-		System.out.println("sf: " +sf);
+		list.add("hello");
+		list.add("Java");
+		list.add("World");
+		System.out.println("list.size : " + list.size());
+		System.out.println("list: "+ list);
 		
-		sf.delete(4, 8);
-		System.out.println("sf: " +sf);
+		list.add(2,"Programming");
+		System.out.println("list : "+ list);
 		
-		StringBuilder sb = new StringBuilder("JAVA World!!");
-		System.out.println("sb: "+sb);
+		list.set(1, "C");
+		System.out.println("list : " + list);
+		
+		String str = list.get(2);
+		System.out.println("list.get(2) : " + str);
+		System.out.println("list: " + list);
+		
+		str = list.remove(2);
+		System.out.println("list.remove(2) : "+ str);
+		System.out.println("list : " + list);
+		
+		list.clear();
+		System.out.println("list: " + list);
+		
+		boolean b = list.isEmpty();
+		System.out.println("list.isEmpty(): "+b);
+		
 	}
 }
